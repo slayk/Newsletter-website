6c9206a11e958cbb802d9b7f1b177261070383ba
Kriti <kriti12304@gmail.com>
2021-04-21 15:20:16 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const mailchimp = require('@mailchimp/mailchimp_marketing');
+const https = require("https");
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+mailchimp.setConfig({
+  apiKey: "caf1fafc8f84933ff5a2ec468da58c0f-us1",
+  server: "us1"
+})
+
+app.post('/', async (req, res) => {
+  const firstName = req.body.fname;
+  const phoneNumber = req.body.pnumber;
+  const emailAddress = req.body.emailid
+
+  const data = {
+    members: [{
+      email_address: emailAddress,
+      status: 'subscribed',
+      merge_fields: {
+        FNAME: firstName,
+        PHONE: phoneNumber
+      }
+    }]
+  }
+
+  const response = await mailchimp.lists.batchListMembers('416bb51a5e', data);
+
+  if (response.error_count === 0) {
+    res.sendFile(__dirname + "/success.html");
+  } else {
+    res.sendFile(__dirname + "/failure.html")
+  }
+
+
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+})
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server started on port 3000");
+});
